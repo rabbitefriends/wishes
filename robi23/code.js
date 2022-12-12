8bfae8214d8de8fe26be7fa02b45cfe16361ef66
Paolo Tamagnini <rabbitefriends@gmail.com>
2022-12-12 18:25:42 +0100

    lol

--- robi23/code.js
+++ robi23/code.js
@@ -1,26 +1,26 @@
 
 // Create data
 var data = [ 
-{name:"Ella", path:"/files/00_ella.mp4", city:"London", type: "video"},
-{name:"Ila & Paolo", path:"/files/01_ila_paolo.ogg", city:"Berlin", type: "audio"},
-{name:"Giusi", path:"/files/02_giusi.mp4", city:"Rome", type: "audio"},
-{name:"Nas & Terenz", path:"/files/03_nas_terenz.ogg", city:"London", type: "audio"},
-{name:"Nikklas", path:"/files/04_nikklas.ogg", city:"London", type: "audio"},
-{name:"Gabi", path:"/files/07_gabi.ogg", city:"Rome", type: "audio"},
-{name:"Bea & Joss", path:"/files/06_bea_joss.ogg", city:"London", type: "audio"},
-{name:"Sauve", path:"/files/05_sauve.ogg", city:"Berlin", type: "audio"},
-{name:"Conni", path:"/files/08_conni.ogg", city:"Rome", type: "audio"},
-{name:"Marco", path:"/files/09_marco.ogg", city:"Rome", type: "audio"},
-{name:"Egemen", path:"/files/10_egemen.ogg", city:"Berlin", type: "audio"},
-{name:"Dennis", path:"/files/12_dennis.ogg", city:"London", type: "audio"},
-{name:"Lalli", path:"/files/11_lalli.ogg", city:"London", type: "audio"},
-{name:"Gerta & Luca", path:"/files/14_gerta.ogg", city:"London", type: "audio"},
-{name:"Lollo", path:"/files/17_lollo.ogg", city:"Berlin", type: "audio"},
-{name:"Lavi", path:"/files/16_lavi.ogg", city:"Berlin", type: "audio"},
-{name:"Fede T.", path:"/files/15_fede_t.aac", city:"Rome", type: "audio"},
-{name:"Brian & Andrea", path:"/files/18_brian_andrea.ogg", city:"Rome", type: "audio"},
-{name:"Asia", path:"/files/13_asia.ogg", city:"Rome", type: "audio"},
-{name:"Fede P.", path:"/files/18_fede_p.ogg", city:"Rome", type: "audio"}]
+{name:"Ella", path:"robi23/files/00_ella.mp4", city:"London", type: "video"},
+{name:"Ila & Paolo", path:"robi23/files/01_ila_paolo.ogg", city:"Berlin", type: "audio"},
+{name:"Giusi", path:"robi23/files/02_giusi.mp4", city:"Rome", type: "audio"},
+{name:"Nas & Terenz", path:"robi23/files/03_nas_terenz.ogg", city:"London", type: "audio"},
+{name:"Nikklas", path:"robi23/files/04_nikklas.ogg", city:"London", type: "audio"},
+{name:"Gabi", path:"robi23/files/07_gabi.ogg", city:"Rome", type: "audio"},
+{name:"Bea & Joss", path:"robi23/files/06_bea_joss.ogg", city:"London", type: "audio"},
+{name:"Sauve", path:"robi23/files/05_sauve.ogg", city:"Berlin", type: "audio"},
+{name:"Conni", path:"robi23/files/08_conni.ogg", city:"Rome", type: "audio"},
+{name:"Marco", path:"robi23/files/09_marco.ogg", city:"Rome", type: "audio"},
+{name:"Egemen", path:"robi23/files/10_egemen.ogg", city:"Berlin", type: "audio"},
+{name:"Dennis", path:"robi23/files/12_dennis.ogg", city:"London", type: "audio"},
+{name:"Lalli", path:"robi23/files/11_lalli.ogg", city:"London", type: "audio"},
+{name:"Gerta & Luca", path:"robi23/files/14_gerta.ogg", city:"London", type: "audio"},
+{name:"Lollo", path:"robi23/files/17_lollo.ogg", city:"Berlin", type: "audio"},
+{name:"Lavi", path:"robi23/files/16_lavi.ogg", city:"Berlin", type: "audio"},
+{name:"Fede T.", path:"robi23/files/15_fede_t.aac", city:"Rome", type: "audio"},
+{name:"Brian & Andrea", path:"robi23/files/18_brian_andrea.ogg", city:"Rome", type: "audio"},
+{name:"Asia", path:"robi23/files/13_asia.ogg", city:"Rome", type: "audio"},
+{name:"Fede P.", path:"robi23/files/18_fede_p.ogg", city:"Rome", type: "audio"}]
 
 var missing = []
 
