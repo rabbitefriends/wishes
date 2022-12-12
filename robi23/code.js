085e90cef9d9739caf3f81e403fc760443250d68
Paolo Tamagnini <rabbitefriends@gmail.com>
2022-12-12 18:36:02 +0100

    lol

--- robi23/code.js
+++ robi23/code.js
@@ -37,7 +37,7 @@ var dict = {
 };
 
 var divvo = d3.select("#friends");
-var root = "https://rabbitefriends.github.io/wishes/robi26"
+var root = "https://rabbitefriends.github.io/wishes/robi23"
 
 const btn = document.querySelector('#btn');        
 const radioButtons = document.querySelectorAll('input[name="size"]');
